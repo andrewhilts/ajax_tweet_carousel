ed6a3318a1eb988b9a4257d34ceae16ef084d345
ahilts <andrew.hilts@utoronto.ca>
2011-07-23 16:15:54 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us">
+<head>
+<title>Ajax Tweet Ticker</title>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js">
+</script>
+<script type="text/javascript" src="js/jquery.jcarousel.js"></script>
+<script type="text/javascript" src="js/tweetRetriever.js"></script>
+<!--
+  jCarousel skin stylesheet
+-->
+<link rel="stylesheet" type="text/css" href="css/tweet-retriever-skin.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<script type="text/javascript">
+$(document).ready(function(){
+  <?php 
+  if(isset($_GET['v'])){
+  if($_GET['v'] == 'f'){echo "vert=false;";}
+  else {echo "vert=true;";}
+  }
+  else {echo "vert=false;";}
+  ?>
+    tweetRetriever.init({
+        user: "InternetRadio",
+        tempo: 60,
+        tweets: 50,
+        container: "#tweets",
+        displayVert: vert,
+        //the below only sets the css height of the tweet container to be n*itemHeight+itemMargin*(n-1). If displayVert is false, use width/hoz instead of height/vert. ItemMargin only assigns a bottom or right margin to the item, depending on displayVert. Otherwise, delete these last params and set everything with CSS (by default, commented out in the css file)
+        displayItemsNum: 3,
+        itemWidth: 300,
+        itemHeight: 60,
+        itemMargin:10
+    });
+});  
+</script>
+  </head>
+  <body>
+  <div id="wrapper">
+  <h1>Ajax Tweet Carousel</h1>
+      <aside><a href="https://github.com/andrewhilts/ajax_tweet_carousel" title="Fork me on Github!"><img src="https://github.com/images/modules/dashboard/bootcamp/octocat_fork.png" alt="Fork me on Github!">Fork me on Github</a></aside>
+      <p>The default user is &ldquo;Internet Radio&rdquo;, the most prolific Twitter account (so the dynamic refreshing of the list is made apparent). By default, the system queries Twitter's API every 2 minutes for the latest 5 tweets (We can't query more than 100 times an hour). 
+      <p>This system will check to see which of the currently displayed tweets are different from the latest ones, and replace those that are no longer in the latest 5 with the new tweets. 
+      <h2>Latest Tweets from <em>InternetRadio</em></h2>
+  <!-- Ensure your skin's classname begins with jcarousel-skin- !-->
+  <ul id="tweets" class="jcarousel-skin-tweet-retriever"></ul>
+   <p>Easily configure either <a href="?v=f">horizontal</a> or <a href="?v=t">vertical</a> presentations.</a>
+   <div class="based-on">
+   <h2>Powered by</h2>
+   <h3><a href="http://sorgalla.com/jcarousel/">JCarousel</a></h3>
+   <p>By Jan Sorgalla. MIT/GPL licence.
+   <p class="desc">jCarousel is a jQuery plugin for controlling a list of items in horizontal or vertical order. The items, which can be static HTML content or loaded with (or without) AJAX, can be scrolled back and forth (with or without animation).
+   <p><a href="http://sorgalla.com/jcarousel/">http://sorgalla.com/jcarousel/</a>
+   <h2>Inspired by</h2>
+   <h3><a href="http://juitter.com/">Juitter</a></h3>
+   <p>By Rodrigo Fanto. MIT licence.
+   <p class="desc">jQuery Plugin to put Twitter live on your website
+   <p><a href="http://juitter.com/">http://juitter.com/</a></p>
+   </div>
+   <h2>Initializing the Tweet Retriver</h2>
+   <pre>
+tweetRetriever.init({
+    user: "InternetRadio",
+    tempo: 60,
+    tweets: 50,
+    container: "#tweets",
+    displayVert: <?php 
+if(isset($_GET['v'])){
+if($_GET['v'] == 'f'){echo "false";}
+else {echo "true";}
+}
+else {echo "false";}
+?>,
+    displayItemsNum: 3,
+    itemWidth: 300,
+    itemHeight: 60,
+    itemMargin:10
+});
+
+//HTML
+
+ul id="tweets" class="jcarousel-skin-tweet-retriever"
+   </pre>
+   <ul class="params-explain">
+    <li><em>user</em> Twitter user whose stream is displayed.</li>
+    <li><em>tempo</em> Rate (s) at which Twitter is queried. (Min 60 in prod env)</li>
+    <li><em>tweets</em> Number of tweets requested</li>
+    <li><em>container</em> ID of UL container for Tweet LIs.</li>
+    <li><em>displayVert</em> Whether or not to display the tweet list vertically.</li>
+    <p>The following are optional params that could otherwise be specified with CSS. Delete them in that case. They're just meant to simplify the sometimes tricky jCarousel config:</p>
+    <li><em>displayItemsNum</em> Number of Tweets to display at once</li>
+    <li><em>itemWidth</em> Width of a Tweet Item</li>
+    <li><em>itemHeight</em> Height of a Tweet Item</li>
+    <li><em>itemMargin</em> Margin between Tweet Items</li>
+  </ul>
+  </div>
+  </body>
+</html>
---
+++ js/tweetRetriever.js
@@ -0,0 +1,262 @@
+tweetRetriever = {
+  newTweets: [],
+  oldTweets: [],
+  addTweets: [],
+  newTweetIDs: [],
+  initialized: false,
+  //function that queries Twitter periodically and updates the Tweet arrays
+  query: function () {
+    $.ajax({
+      crossDomain:true,
+      url: "http://twitter.com/status/user_timeline/"+this.user+".json?count="+this.quantity+"&rpp=5",
+      type: 'GET',
+  	  dataType: 'jsonp',
+  	  timeout: 3000,
+  	  error: function(jqXHR, textStatus, errorThrown){alert(errorThrown);},
+      success:  function(json){
+        tweetRetriever.oldTweets = tweetRetriever.newTweets;
+        tweetRetriever.newTweets = [];
+        tweetRetriever.newTweetIDs = [];
+        $.each(json,function(i,item) {
+          tweetRetriever.newTweets.push([item.id,item]);
+        });
+        tweetRetriever.compareTweetQueries();
+        if (!tweetRetriever.initialized){
+          tweetRetriever.displayInit();
+        }
+      }   
+    });
+    this.update();
+  },
+  //Period at which the query function is called; hence when the tweets will be updated. Don't do more than one per minute, or Twitter's API will kick your IP out for an hour.
+  update: function () {
+    setTimeout("tweetRetriever.query()",this.tempo);
+  },
+  //Function that sets up the TweetRetriever object, launches the first query
+  init: function (params) {
+    this.tempo = params.tempo*1000;
+    this.quantity = params.tweets;
+    this.user = params.user;
+    this.container = params.container;
+    this.skin =$(params.container)[0].className;
+    
+    if(params.displayVert == null){
+        this.displayVert = true;
+    }
+    else{
+        this.displayVert = params.displayVert;
+    }
+    totalMarginSpace = params.itemMargin*(params.displayItemsNum-1);
+    if(params.itemWidth !== null && params.itemHeight !== null && this.displayVert === true){
+       containerHeight = params.itemHeight*params.displayItemsNum+totalMarginSpace;
+       $("<style type='text/css'>\
+            ."+this.skin+" .jcarousel-container-vertical,\
+            .jcarousel-skin-tweet-retriever .jcarousel-clip-vertical{\
+                width:"+params.itemWidth+"px;\
+                height:"+containerHeight+"px;\
+                }\
+            ."+this.skin+" .jcarousel-item{\
+                width:"+params.itemWidth+"px;\
+                height:"+params.itemHeight+"px;\
+                }\
+            ."+this.skin+" .jcarousel-item-vertical{\
+                margin-bottom:"+params.itemMargin+"px;\
+                }\
+          </style>").appendTo("head");
+    }
+    else if(params.itemWidth !== null && params.itemHeight !== null && this.displayVert === false){
+       containerWidth = params.itemWidth*params.displayItemsNum+totalMarginSpace;
+       $("<style type='text/css'>\
+            ."+this.skin+" .jcarousel-container-horizontal,\
+            ."+this.skin+" .jcarousel-clip-horizontal{\
+                width:"+containerWidth+"px;\
+                height:"+params.itemHeight+"px;\
+                }\
+            ."+this.skin+" .jcarousel-item{\
+                width:"+params.itemWidth+"px;\
+                height:"+params.itemHeight+"px;\
+                }\
+            ."+this.skin+" .jcarousel-item-horizontal{\
+                margin-right:"+params.itemMargin+"px;\
+                margin-left:0\
+                }\
+          </style>").appendTo("head");
+    }
+    this.query();
+  },
+  //Function that sets up how the TweetRetriever is displayed. In this case it's a JCarousel
+  displayInit: function () {
+    $.each(this.addTweets,function(i,item){
+      tweet_time = tweetRetriever.timeAgo(item.created_at);
+      tweet = tweetRetriever.tweetFormat(item);
+      $(tweetRetriever.container).prepend(tweet);
+    });
+    this.initialized = true;
+    displayVert = this.displayVert;
+    $(tweetRetriever.container).jcarousel({
+      auto: 5,
+      animation:1000,
+      scroll: 1,
+      wrap: 'circular',
+      vertical:displayVert,
+      initCallback: mycarousel_initCallback,
+      itemLoadCallback: display,
+      itemFirstOutCallback: {
+	      onBeforeAnimation: callback1
+	    },
+	    itemLastInCallback: {
+	      onBeforeAnimation: callback2
+	    }
+    });  
+  },
+  //Compares the old and new tweet sets, to find new ones to place in the addTweets array
+  compareTweetQueries: function () {
+    seen = [], diff = [], newTweets = [], oldTweets = [];
+    $.each(this.newTweets,function(i,item){
+      newTweets.push(item[0]);
+    });
+    $.each(this.oldTweets,function(i,item){
+      oldTweets.push(item[0]);
+    });
+    for (var i = 0; i < oldTweets.length; i++)
+        seen[oldTweets[i]] = true;
+    for (var i = 0; i < newTweets.length; i++)
+        if (!seen[newTweets[i]])
+            diff.push(this.newTweets[i][1]);
+    this.addTweets = diff.reverse();
+  },
+  //Format the tweet text, create links. Taken from Juitter.
+  textFormat: function(texto){
+		//This from Juitter
+		//make links
+		var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+		texto = texto.replace(exp,"<a href='$1' class='extLink'>$1</a>"); 
+		var exp = /[\@]+([A-Za-z0-9-_]+)/ig;
+		texto = texto.replace(exp,"<a href='http://twitter.com/$1' class='profileLink'>@$1</a>"); 
+		var exp = /[\#]+([A-Za-z0-9-_]+)/ig;
+		texto = texto.replace(exp,"<a href='http://twitter.com/search?q=%23#$1'' class='hashLink'>#$1</a>"); 
+		// make it bold
+		return texto;
+	},
+	//Render the tweet contents for display.
+	tweetFormat: function(item){
+	  tweet = '<li>'+'<a href="http://www.twitter.com/'+item.user.name+'" title="'+item.user.name+'&rsquo;s Twitter page">';
+    tweet += '<img src="'+item.user.profile_image_url+'" alt="'+item.user.name+'&rsquo;s profile picture"/></a>'
+    tweet += '<span class="name"><a href="http://www.twitter.com/'+item.user.name+'" title="'+item.user.name+'&rsquo;s Twitter page">'+item.user.name+'</a></span>';
+    tweet += '<span class="date">'+tweet_time+'</span>';
+    tweet += '<p>'+tweetRetriever.textFormat(item.text)+'</p>';
+    tweet += '<span class="real_time">'+item.created_at+'</span></li>';
+    return tweet;
+	},
+	//Calculate the relative time of the tweet
+	timeAgo : function(dateString) {
+	    var browser = function() {
+            var ua = navigator.userAgent;
+            return {
+              ie: ua.match(/MSIE\s([^;]*)/)
+            };
+          }();
+      var rightNow = new Date();
+      var then = new Date(dateString);
+
+      if (browser.ie) {
+        // IE can't parse these crazy Ruby dates
+        then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
+      }
+
+      var diff = rightNow - then;
+
+      var second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24,
+          week = day * 7;
+
+      if (isNaN(diff) || diff < 0) {
+        return ""; // return blank string if unknown
+      }
+
+      if (diff < second * 2) {
+        // within 2 seconds
+        return "right now";
+      }
+
+      if (diff < minute) {
+        return Math.floor(diff / second) + " seconds ago";
+      }
+
+      if (diff < minute * 2) {
+        return "about 1 minute ago";
+      }
+
+      if (diff < hour) {
+        return Math.floor(diff / minute) + " minutes ago";
+      }
+
+      if (diff < hour * 2) {
+        return "about 1 hour ago";
+      }
+
+      if (diff < day) {
+        return  Math.floor(diff / hour) + " hours ago";
+      }
+
+      if (diff > day && diff < day * 2) {
+        return "yesterday";
+      }
+
+      if (diff < day * 365) {
+        return Math.floor(diff / day) + " days ago";
+      }
+
+      else {
+        return "over a year ago";
+      }
+
+    }
+  };
+  function callback1(jcarousel_object, li_object, index, state){
+	  	$(li_object).fadeTo(500,0.01);
+	}
+	function callback2(jcarousel_object, li_object, index, state){
+	    //initial last visible item doesn't do the fade IN animation
+	    if(state!=="init"){
+	    	$(li_object).fadeTo(1,0.01).delay(500).fadeTo(500,1);
+	    }
+	}
+	var index = "";
+	function display(carousel,state){
+	  if (state == 'init') { 
+      orig_size = carousel.size();
+      index = orig_size;
+    }
+	  else{
+	    real_times = $(tweetRetriever.container).find('span.real_time');
+       for(i in real_times){
+         if(!isNaN(i)){
+           new_time = real_times[i].innerHTML;
+           old_time = $(real_times[i]).parent().children('span.date');
+           $(old_time[0]).html(tweetRetriever.timeAgo(new_time));
+         }
+       }
+      if(tweetRetriever.addTweets.length > 0){
+  	     $.each(tweetRetriever.addTweets,function(i,item){
+  	        tweet_time = tweetRetriever.timeAgo(item.created_at);
+            tweet = tweetRetriever.tweetFormat(item);
+            carousel.add(index,tweet);
+            index-=1;
+            if(index==0){index=orig_size;} 
+  	     });
+  	     tweetRetriever.addTweets = [];
+  	  }
+    }
+	}
+function mycarousel_initCallback(carousel)
+{
+    // Pause autoscrolling if the user moves with the cursor over the clip.
+    carousel.clip.hover(function() {
+        carousel.stopAuto();
+    }, function() {
+        carousel.startAuto();
+    });
+};
