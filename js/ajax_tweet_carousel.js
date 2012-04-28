52c470745f6f012e20488fecf2ee3973f92952db
Andrew Hilts <andrewchilts@gmail.com>
2012-04-28 18:32:27 -0400

    add event model

--- js/ajax_tweet_carousel.js
+++ js/ajax_tweet_carousel.js
@@ -1,4 +1,57 @@
+//Copyright (c) 2010 Nicholas C. Zakas. All rights reserved.
+//MIT License
+
+function EventTarget(){
+    this._listeners = {};
+}
+
+EventTarget.prototype = {
+
+    constructor: EventTarget,
+
+    addListener: function(type, listener){
+        if (typeof this._listeners[type] == "undefined"){
+            this._listeners[type] = [];
+        }
+
+        this._listeners[type].push(listener);
+    },
+
+    fire: function(event){
+        if (typeof event == "string"){
+            event = { type: event };
+        }
+        if (!event.target){
+            event.target = this;
+        }
+
+        if (!event.type){  //falsy
+            throw new Error("Event object missing 'type' property.");
+        }
+
+        if (this._listeners[event.type] instanceof Array){
+            var listeners = this._listeners[event.type];
+            for (var i=0, len=listeners.length; i < len; i++){
+                listeners[i].call(this, event);
+            }
+        }
+    },
+
+    removeListener: function(type, listener){
+        if (this._listeners[type] instanceof Array){
+            var listeners = this._listeners[type];
+            for (var i=0, len=listeners.length; i < len; i++){
+                if (listeners[i] === listener){
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+};
+
 function TweetCarousel(params,jcarouselParams){
+  EventTarget.call(this);
   this.newTweets = [];
   this.oldTweets = [];
   this.addTweets = [];
@@ -27,6 +80,8 @@ function TweetCarousel(params,jcarouselParams){
     this.stylize(params);
   }
 }
+
+TweetCarousel.prototype = new EventTarget();
 
 TweetCarousel.prototype.update = function (){
   this.timeLeft--;
@@ -225,6 +280,7 @@ TweetCarousel.prototype.displayInit = function(){
     this.container.jcarousel(this.jcarouselParams);
     this.carouselUpdateStarted = false;
   }
+  this.fire("displayInit");
 }
 
 TweetCarousel.prototype.timeAgo = function(dateString) {
