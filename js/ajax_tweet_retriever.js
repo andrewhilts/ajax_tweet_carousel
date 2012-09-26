d737248d76cd38102552fd4d25090477db123a67
Andrew Hilts <ahilts@hhmedia.com>
2012-09-26 13:00:41 -0400

    Twitter Search/User models in place and basically working

--- js/ajax_tweet_retriever.js
+++ js/ajax_tweet_retriever.js
@@ -7,15 +7,44 @@ Ajax TweetRetriever is distributed in the hope that it will be useful, but WITHO
 
 To review a copy of the GNU General Public License, see http://www.gnu.org/licenses/.
 */
+var TwitterUtils = {};
+TwitterUtils.textFormat = function(texto){
+  /*
+  This function copied from Juitter.
+  BY RODRIGO FANTE - http://rodrigofante.com
+  Juitter is distributed under the MIT License
+  Read more about the MIT License --> http://www.opensource.org/licenses/mit-license.php
+  */
+
+  //make links
+  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  texto = texto.replace(exp,"<a href='$1' class='extLink'>$1</a>");
+  var exp = /[\@]+([A-Za-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF0-9-_]+)/ig;
+  uri = encodeURIComponent(texto.match(exp));
+  uri = uri.substring(3);
+  texto = texto.replace(exp,"<a href='http://twitter.com/"+uri+"' class='profileLink'>@$1</a>");
+  var exp = /[\#]+([A-Za-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF0-9-_]+)/ig;
+  uri = encodeURIComponent(texto.match(exp));
+  uri = uri.substring(3);
+  texto = texto.replace(exp,"<a href='http://twitter.com/search?q=%23"+uri+"' class='hashLink'>#$1</a>");
+  // make it bold
+  return texto;
+};
+TwitterUtils.template = '<li><a href="{{userLink}}" title="{{name}}&rsquo;s Twitter page" class="profilePicLink"><img src="{{image}}" alt="{{name}}&rsquo;s profile picture"></a><div class="tweetControls"><a href="{{tweet_replyLink}}" class="tweetControl reply" title="Reply"><i></i></a><a href="{{tweet_reTweetLink}}" class="tweetControl retweet" title="Retweet"><i></i></a><a href="{{tweet_favouriteLink}}" class="tweetControl favorite" title="Favourite"><i></i></a></div><span class="name"><a href="{{userLink}}" title="{{name}}&rsquo;s Twitter page">{{name}}</a></span><span class="date">{{timeAgo}}</span><p>{{{text}}}</p></li>';
+
 
 function socialMediaAPIModel(params){
-  this.baseURL = params.baseURL;
-  this.allowedParams = params.allowedParams;
-  this.responseObjectModel = params.responseObjectModel;
-  this.getItems = params.getItems;
+  this.baseURL = params.baseURL || false;
+  this.allowedParams = params.allowedParams || false;
+  this.responseObjectModel = params.responseObjectModel || false;
+  this.getItems = params.getItems || false;
+  this.template = params.template || false;
+  this.parseData = params.parseData || false;
   //provide set of accepted params (but actual param values build in retriever)
 }
-
+socialMediaAPIModel.prototype.init = function(){
+  return this;
+};
 socialMediaAPIModel.prototype.buildURL = function(URLParams){
   url = this.baseURL;
   count = 0;
@@ -103,8 +132,8 @@ socialMediaAPIModel.prototype.buildURL = function(URLParams){
     return false;
   }
 };
-
-TwitterAPIModel = new socialMediaAPIModel({
+function TwitterSearchAPIModel(){}
+TwitterSearchAPIModel.prototype = new socialMediaAPIModel({
   //See https://dev.twitter.com/docs/api/1/get/search for param descriptions
   baseURL: "http://search.twitter.com/search.json",
   allowedParams: [
@@ -170,26 +199,140 @@ TwitterAPIModel = new socialMediaAPIModel({
       type : "boolean",
       required : false
     }
-  ]
+  ],
+  getItems: function(json){
+    return json.results;
+  },
+  template: TwitterUtils.template,
+  parseData: function(data,parent){
+    newData = {
+      name: data.from_user,
+      username: data.from_user_name,
+      userLink: "http://www.twitter.com/"+data.from_user_name,
+      image: data.profile_image_url,
+      tweet_id: data.id_str,
+      tweet_replyLink: "http://twitter.com/intent/tweet?in_reply_to="+data.id_str,
+      tweet_reTweetLink: "http://twitter.com/intent/retweet?tweet_id="+data.id_str,
+      tweet_favouriteLink: "http://twitter.com/intent/favorite?tweet_id="+data.id_str,
+      timestamp: data.created_at,
+      timeAgo: parent.timeAgo(data.created_at),
+      text: this.textFormat(data.text)
+    };
+    return newData;
+  }
 });
+TwitterSearchAPIModel.prototype.textFormat = TwitterUtils.textFormat;
 
-var PinterestAPIModel = new socialMediaAPIModel({
+function TwitterUserAPIModel(){}
+TwitterUserAPIModel.prototype = new socialMediaAPIModel({
+  // see https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
+  baseURL: "http://twitter.com/status/user_timeline/",
+  allowedParams: [
+    {
+      key : "since_id",
+      type : "number",
+      required : false
+    },
+    {
+      key : "max_id",
+      type : "number",
+      required : false
+    },
+    {
+      key : "page",
+      type : "number",
+      required : false
+    },
+    {
+      key : "count",
+      type : "number",
+      required : false
+    },
+    {
+      key : "show_user",
+      type : "boolean",
+      required : false
+    },
+    {
+      key : "contributor_details",
+      type : "boolean",
+      required : false
+    },
+    {
+      key : "max_id",
+      type : "number",
+      required : false
+    },
+    {
+      key : "include_entities",
+      type : "boolean",
+      required : false
+    }
+  ],
+  getItems: function(json){
+    return json;
+  },
+  template: TwitterUtils.template,
+  parseData: function(data,parent){
+    newData = {
+      name: data.user.name,
+      username: data.user.screen_name,
+      userLink: "http://www.twitter.com/"+data.user.screen_name,
+      image: data.user.profile_image_url,
+      tweet_id: data.id_str,
+      tweet_replyLink: "http://twitter.com/intent/tweet?in_reply_to="+data.id_str,
+      tweet_reTweetLink: "http://twitter.com/intent/retweet?tweet_id="+data.id_str,
+      tweet_favouriteLink: "http://twitter.com/intent/favorite?tweet_id="+data.id_str,
+      timestamp: data.created_at,
+      timeAgo: parent.timeAgo(data.created_at),
+      text: this.textFormat(data.text)
+    };
+    return newData;
+  }
+});
+TwitterUserAPIModel.prototype.init = function(URLParams){
+  this.baseURL = this.baseURL + URLParams.username + '.json';
+};
+TwitterUserAPIModel.prototype.textFormat = TwitterUtils.textFormat;
+function PinterestAPIModel (){}
+PinterestAPIModel.prototype = new socialMediaAPIModel({
   baseURL: "http://query.yahooapis.com/v1/public/yql?q=select%20channel%20from%20xml%20where%20url%3D'http%3A%2F%2Fpinterest.com%2F",
   getItems: function(json){
     return json.query.results.rss.channel.item;
+  },
+  template: '<li><a href="{{link}}"><span class="img" style="background:url({{image}}); background-size:45px; background-repeat:no-repeat; position:relative; display:block; width:45px; height:45px;">&nbsp;</span><span class="date">{{timeAgo}}</span><p>{{{description}}}</p></a></li>',
+  parseData: function(data,parent){
+    var data2parse = $(data.description);
+    data.image = $(data2parse.get(0)).children('a').children('img').attr('src');
+    data.description = data2parse[1].innerHTML;
+    data.timestamp = data.pubDate;
+    data.timeAgo = parent.timeAgo(data.pubDate);
+    return data;
   }
 });
+
 //Override default buildURL function because Pinterst doesn't have a real API
-PinterestAPIModel.buildURL = function(username){
-  return this.baseURL+username+"%2Ffeed.rss'&format=json";
+PinterestAPIModel.prototype.buildURL = function(URLParams){
+  return this.baseURL+URLParams.username+"%2Ffeed.rss'&format=json";
 };
-function SocialMediaAPIRetriever(params){
+function SocialMediaAPIWidget(params){
   console.log(params.URLParams);
   this.model = params.model;
+  var that = this;
+  this.model.init(params.URLParams);
   this.url = this.model.buildURL(params.URLParams);
+  this.quantity = 4;
+  this.query();
+  $(this).bind('receive',function(){
+    for(var i=0; i < this.quantity; i++){
+      this.data[i] = this.model.parseData(this.data[i],that);
+      console.log(this.data[i]);
+      $("#test").append(Mustache.render(this.model.template,this.data[i]));
+    }
+  });
 }
 
-SocialMediaAPIRetriever.prototype.query = function(){
+SocialMediaAPIWidget.prototype.query = function(){
   $.ajax({
     crossDomain:true,
     url: this.url,
@@ -200,10 +343,66 @@ SocialMediaAPIRetriever.prototype.query = function(){
       
     }.bind(this),
     success:  function(json){
-      console.log(this.model.getItems(json));
+      this.data = this.model.getItems(json);
+      $(this).trigger('receive');
     }.bind(this)
   });
-}
+};
+
+SocialMediaAPIWidget.prototype.timeAgo = function(dateString) {
+  var browser = function() {
+        var ua = navigator.userAgent;
+        return {
+          ie: ua.match(/MSIE\s([^;]*)/)
+        };
+      }();
+  var rightNow = new Date();
+  var then = new Date(dateString);
+  if (browser.ie) {
+    // IE can't parse these crazy Ruby dates
+    then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
+  }
+
+  var diff = rightNow - then;
+  var second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24,
+      week = day * 7;
+
+  if (isNaN(diff)) {
+    return ""; // return blank string if unknown
+  }
+  if (diff < second * 2 || diff < 0) {
+    // within 2 seconds
+    return "right now";
+  }
+  if (diff < minute) {
+    return Math.floor(diff / second) + " seconds ago";
+  }
+  if (diff < minute * 2) {
+    return "about 1 minute ago";
+  }
+  if (diff < hour) {
+    return Math.floor(diff / minute) + " minutes ago";
+  }
+  if (diff < hour * 2) {
+    return "about 1 hour ago";
+  }
+  if (diff < day) {
+    return  Math.floor(diff / hour) + " hours ago";
+  }
+  if (diff > day && diff < day * 2) {
+    return "yesterday";
+  }
+  if (diff < day * 365) {
+    return Math.floor(diff / day) + " days ago";
+  }
+  else {
+    return "over a year ago";
+  }
+};
+
 
 function TweetRetriever(params){
   EventTarget.call(this);
@@ -306,7 +505,7 @@ TweetRetriever.prototype.query = function() {
     }.bind(this)
   });
   this.update();
-}
+};
 
 TweetRetriever.prototype.saveTweets = function(json, queryType){
   switch(queryType){
@@ -346,7 +545,7 @@ TweetRetriever.prototype.saveTweets = function(json, queryType){
       text: item.text,
       created_at: item.created_at,
       user: user
-    }
+    };
     tweet = [item.id, tweetData];
     this.newTweets.push(tweet);
   }.bind(this));
@@ -457,7 +656,7 @@ TweetRetriever.prototype.timeAgo = function(dateString) {
 
 TweetRetriever.prototype.tweetFormat = function(item){
   tweet = '<a href="http://www.twitter.com/'+item.user.name+'" title="'+item.user.name+'&rsquo;s Twitter page" class="profilePicLink">';
-  tweet += '<img src="'+item.user.profile_image_url+'" alt="'+item.user.name+'&rsquo;s profile picture"/></a>'
+  tweet += '<img src="'+item.user.profile_image_url+'" alt="'+item.user.name+'&rsquo;s profile picture"/></a>';
   if(this.controlRetweet === true || this.controlReply === true || this.controlFavorite === true){
       tweet += '<div class="tweetControls">';
 
